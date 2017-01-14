03ae0e303945a998dbb43ed1867e46a15cfc8cae
Romel Sandoval <romel@openmailbox.org>
2017-01-14 11:55:57 -0600

    Added task record type and unit test

--- src/main/records.scm
+++ src/main/records.scm
@@ -1,9 +1,12 @@
 (define-library (plogger records)
   (import (scheme base))
   (export make-project project? project-id project-name
-	  project->alist alist->project)
+	  project->alist alist->project
+	  make-task task? task-id task-name task-desc
+	  task->alist alist->task)
   (begin
-    
+
+    ;; Project  
     (define-record-type project-record
       (make-project id name)
       project?
@@ -17,6 +20,26 @@
     
     (define (alist->project alist)
       (make-project (cdr (assq 'project-id alist))
-		    (cdr (assq 'project-name alist))))))
+		    (cdr (assq 'project-name alist))))
+
+    ;; Task
+    (define-record-type task-record
+      (make-task id name desc)
+      task?
+      (id task-id)
+      (name task-name)
+      (desc task-desc))
+  
+    (define (task->alist task)
+      (list
+       (cons 'task-id (task-id task))
+       (cons 'task-name (task-name task))
+       (cons 'task-desc (task-desc task))))
+    
+    (define (alist->task alist)
+      (make-task (cdr (assq 'task-id alist))
+		 (cdr (assq 'task-name alist))
+		 (cdr (assq 'task-desc alist))))
 
 
+))
