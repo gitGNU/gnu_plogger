46a645a109d2fc8dd732a0a35b131a4e19203e8a
Romel Sandoval <romel.sandoval@nearshoretechnology.com>
2017-04-30 20:49:00 -0500

    Improved kawa installation script

--- install-kawa.sh
+++ install-kawa.sh
@@ -1,11 +1,11 @@
-# TODO check jline3 availability
-apt-get install -y libservlet3.1-java
+apt-get install -y libservlet3.1-java unzip docbook
+cd /opt/
+wget ftp://ftp.gnu.org/pub/gnu/kawa/kawa-2.4.zip
+unzip kawa-2.4.zip
 cd /usr/local/src
 wget ftp://ftp.gnu.org/pub/gnu/kawa/kawa-2.4.tar.gz
 tar -xvzf kawa-2.4.tar.gz
 cd kawa-2.4
-./autogen.sh
-./configure --with-jline3 --with-javafx --with-servlet
+./configure --with-docbook-stylesheet=/usr/share/sgml/docbook/stylesheet/xsl/docbook-xsl --with-jline3=/opt/kawa-2.4/lib/jline.jar --with-javafx --with-servlet=/usr/share/java/servlet-api-3.0.jar
 make
-make check
 make install
