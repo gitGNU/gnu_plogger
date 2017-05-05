1d9eec186dd360f65fcc6d149ffb0596694fa61d
Romel R. Sandoval <romel@openmailbox.org>
2017-05-04 19:10:08 -0500

    Added new models

--- src/main/kawa/models.scm
+++ src/main/kawa/models.scm
@@ -1,12 +1,41 @@
 (define-library (plogger models)
   (import (kawa base))
-  (export <project>)
+  (export <org> <project>)
   (begin
 
-    (define-simple-class <project> ()
+    (define-simple-class <named> ()
+      
       (name ::String)
-      ((*init* (name0 ::String))
-       (set! name name0))
-      ((get-name) name))
+      (alias ::String)
+
+      ((*init*) #!void)
+      
+      ((*init* (name0 ::String) (alias0 ::String))
+       (set! name name0)
+       (set! alias alias0))
+      
+      ((get-name) name)
+      ((get-alias) alias)
+      
+      )
+
+    (define-simple-class <org> (<named>))
+    
+    (define-simple-class <project> ()
+      
+      (name ::String)
+      (alias ::String)
+      (org ::<org>)
+      
+      ((*init* (name0 ::String) (alias0 ::String) (org0 ::<org>))
+       (set! name name0)
+       (set! alias alias0)
+       (set! org org0))
+      
+      ((get-name) name)
+      ((get-alias) alias)
+      ((get-org) org)
+      
+      )
 
 ))
--- src/test/models-test.scm
+++ src/test/models-test.scm
@@ -2,13 +2,30 @@
 (import (class org.junit Assert))
 (import (plogger models))
 
+
+
 (define-simple-class models-tests ()
-  
-  ((test-project) ::void (@Test)
+
+  ((test-org) ::void (@Test)
+   
    (let* ((name "Taco Bell")
 	  (alias "tb")
-	  (project (<project> name)))
+	  (org (<org> name alias)))
+     
+     (Assert:assertEquals name (org:get-name))
+     (Assert:assertEquals alias (org:get-alias))
+     (Assert:assertTrue (<org>? org))))
+  
+  ((test-project) ::void (@Test)
+   
+   (let* ((org-name "Taco Bell")
+	  (org-alias "tb")
+	  (name "SSO Integration")
+	  (alias "tb-sso")
+	  (project (<project> name alias (<org> org-name org-alias)))
+	  (org ::<org> (project:get-org)))
      
      (Assert:assertEquals name (project:get-name))
-     (Assert:assertTrue (<project>? project))
-     (Assert:assertEquals alias (project:get-alias)))))
+     (Assert:assertEquals alias (project:get-alias))
+     (Assert:assertEquals org-alias (org:get-alias))
+     (Assert:assertTrue (<project>? project)))))
