84fbe6a5dd7bae086027abab10e5aada753570a1
Romel R. Sandoval <romel@openmailbox.org>
2017-05-05 07:54:04 -0500

    Refactored org and project classes

--- src/main/kawa/models.scm
+++ src/main/kawa/models.scm
@@ -8,34 +8,27 @@
       (name ::String)
       (alias ::String)
 
-      ((*init*) #!void)
-      
       ((*init* (name0 ::String) (alias0 ::String))
        (set! name name0)
        (set! alias alias0))
       
       ((get-name) name)
-      ((get-alias) alias)
-      
-      )
+      ((get-alias) alias))
 
-    (define-simple-class <org> (<named>))
+    (define-simple-class <org> (<named>)
+      ((*init* (name0 ::String) (alias0 ::String))
+       (invoke-special <named> (this) '*init* name0 alias0)))
     
-    (define-simple-class <project> ()
+    (define-simple-class <project> (<named>)
       
-      (name ::String)
-      (alias ::String)
       (org ::<org>)
       
       ((*init* (name0 ::String) (alias0 ::String) (org0 ::<org>))
-       (set! name name0)
-       (set! alias alias0)
+       (invoke-special <named> (this) '*init* name0 alias0)
        (set! org org0))
       
-      ((get-name) name)
-      ((get-alias) alias)
-      ((get-org) org)
-      
-      )
+      ((get-org) org))
 
-))
+    
+
+    ))
